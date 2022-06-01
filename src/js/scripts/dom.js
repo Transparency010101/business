6a1342267947cf303886bcbe34f8458a02e45c2a
Transparency010101 <ossys1010110@gmail.com>
2022-06-02 00:14:40 +0300

    сделал news

--- src/js/scripts/dom.js
+++ src/js/scripts/dom.js
@@ -1,1 +1,62 @@
 // * тут небольшие DOM/BOM скрипты
+
+// * Фильтер для news {
+const newsNavFilterItem = document.querySelectorAll(".news__nav-filter-item");
+const newsColumnsColumn = document.querySelectorAll(".news-columns__column");
+
+newsNavFilterItem.forEach(link => {
+    link.addEventListener("click", function (e) {
+        let dataFilterCount = link.getAttribute("data-filter");
+        newsColumnsColumn.forEach(column => {
+            if (dataFilterCount == "3" && dataFilterCount == 3){
+                column.classList.add("active");
+            } else  {
+                column.classList.remove("active");
+                if (column.classList.contains("filter_" + dataFilterCount)) {
+                    column.classList.add("active");
+                }
+            }
+        });
+        e.preventDefault();
+    });
+});
+// * Фильтер для news }
+
+// * Меняется flex размер, взависимости от количетва колонок {
+let newsColumnsColumnLenght = newsColumnsColumn.length;
+
+newsColumnsColumn.forEach(el => {
+    switch (newsColumnsColumnLenght) {
+        case 1:
+            el.style.flex = "0 1 100%";
+            break;
+
+        case 3:
+            el.style.flex = "0 1 33.333%";
+            break;
+
+        case 2:
+            el.style.flex = "0 1 50%";
+            break;
+
+        case 3:
+            el.style.flex = "0 1 33.333%";
+            break;
+
+        case 4:
+            el.style.flex = "0 1 25%";
+            break;
+    }
+});
+// * Меняется flex размер, взависимости от количетва колонок }
+
+// * Изменяет отборание ссылок фильтров {
+newsNavFilterItem.forEach(link => {
+    link.addEventListener("click", function() {
+        newsNavFilterItem.forEach(removeLink => {
+            removeLink.classList.remove("active");
+        });
+        link.classList.add("active");
+    });
+});
+// * Изменяет отборание ссылок фильтров }
